8c82edb18f8cfdf559c92aff2e504b2ea04896ea
ffffrank <630656007@qq.com>
2020-09-21 17:27:00 +0800

    '增加router和vuex'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.less'
 
@@ -7,5 +9,7 @@ Vue.config.productionTip = false
 Vue.use(Antd)
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
