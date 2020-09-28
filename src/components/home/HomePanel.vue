f3e9299019e36b17243f84b0321f41ab806e3b3e
ffffrank <630656007@qq.com>
2020-09-28 10:45:00 +0800

    主页panel组件

---
+++ src/components/home/HomePanel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home-panel-wrapper">
+    <div class="header">
+      <p>{{ name }}</p>
+    </div>
+    <div class="body">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePanel",
+  props:{
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .home-panel-wrapper{
+    width: 1000px;
+    height: 1704px;
+    /*background: rgba(255, 255, 255, 0.05);*/
+    /*border: 6px solid rgba(33, 160, 253, 0.1);*/
+  }
+  .home-panel-wrapper .header{
+    width: 1000px;
+    height: 88px;
+    background: linear-gradient(180deg, rgba(9,251,255,0.8) 0%, rgba(24,153,255,0.8) 100%);
+    text-align: center;
+  }
+  .home-panel-wrapper .body{
+    width: 1000px;
+    height: 1616px;
+    background: rgba(255, 255, 255, 0.05);
+    border: 6px solid rgba(33, 160, 253, 0.1);
+  }
+  .home-panel-wrapper .header p{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /*width: 202px;*/
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    font-size: 48px;
+    font-family: PingFangSC-Semibold, PingFang SC,serif;
+    font-weight: 600;
+    margin: auto;
+    color: #FFFFFF;
+    letter-spacing: 1px;
+  }
+</style>
---
+++ src/containers/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home-wrapper">
+    <home-panel v-for="name in names" v-bind:name = "name.value" v-bind:key="name.id"/>
+  </div>
+</template>
+
+<script>
+import HomePanel from "@/components/home/HomePanel";
+export default {
+name: "Home",
+  components: {HomePanel},
+  data:function () {
+    return {
+      names: [{
+        id: 1,
+        value:"化工园区"
+      }, {
+        id: 2,
+        value: "文化创意园区"
+      }, {
+        id: 3,
+        value: "农业园区"
+      }, {
+        id: 4,
+        value:"学校"
+      }, {
+        id: 5,
+        value: "医养健康园区"
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #home-wrapper{
+    width: 6508px;
+    height: 1940px;
+    padding: 0;
+    margin: 0;
+    background: url("../assets/home/background.png");
+  }
+</style>
